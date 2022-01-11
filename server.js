774ba055fc5c6b92c28f97eda9e91989c9ba58b6
lefuller <fullernle@gmail.com>
2022-01-11 15:39:56 -0800

    implements graphql dummy schema to test queries

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const expressGraphQL = require("express-graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
 const app = express();
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'HelloWorld',
+    fields: () => ({
+      message: { 
+				type: GraphQLString, 
+				resolve: () => "Hello World" 
+			},
+    }),
+  }),
+});
 
 app.use(
   "/graphql",
   expressGraphQL.graphqlHTTP({
+		schema: schema,
     graphiql: true,
   })
 );
