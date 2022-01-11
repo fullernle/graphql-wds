affdc7abdf9c93ced138a82ca873a43b8cf1bd51
lefuller <fullernle@gmail.com>
2022-01-10 17:17:00 -0800

    imports in expressGraphQL and creates root /graphql

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require("express");
+const expressGraphQL = require("express-graphql");
 const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphQL.graphqlHTTP({
+    graphiql: true,
+  })
+);
 
 app.listen(5000, () => console.log("Server running"));
